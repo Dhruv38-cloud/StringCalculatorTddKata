c81cf740b337fb2f97672f2759286e54e3147fcb
Dhruv Verma <dhruvverma015@gmail.com>
2024-04-24 00:02:16 +0530

    Added to handle numberic string to number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,7 @@
 class StringCalculator
   def self.add(num_string)
     return 0 if num_string.empty?
+
+    num_string.to_i
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe 'StringCalculator' do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'return number for an its numbric string' do
+      expect(StringCalculator.add("6")).to eq(6)
+    end
   end
 end
