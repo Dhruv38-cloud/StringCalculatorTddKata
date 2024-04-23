831ace140a0c577294b467ca128b80ce995a092a
Dhruv Verma <dhruvverma015@gmail.com>
2024-04-24 01:46:36 +0530

    Allow the add method to handle new lines between numbers instead of commas

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,12 +1,23 @@
 class StringCalculator
   def self.add(num_string)
     return 0 if num_string.empty?
+    
+    return num_string.to_i if is_a_number?(num_string)
 
-    if num_string.include?(',')
-      numbers = num_string.split(',').map(&:to_i)
-      return numbers.sum
+    numbers = num_string.split(/[,\n]/)
+    raise ArgumentError, 'Invalid input' if numbers&.any?(&:empty?)
+    
+    return numbers.map(&:to_i).sum
+  end
+
+  private
+
+  def self.is_a_number?(string)
+    begin
+      Integer(string)
+      true
+    rescue ArgumentError
+      false
     end
-
-    num_string.to_i
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe 'StringCalculator' do
     it 'return calculated number for an its numbric string which is saparated by comma' do
       expect(StringCalculator.add("1,2,3")).to eq(6)
     end
+
+    it 'return calculated number for an its numbric string with new line which is saparated by comma' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+    
+    it 'return invalid input for an its numbric string which is saparated by comma' do
+      expect {StringCalculator.add("1,\n2")}.to raise_error(ArgumentError)
+    end
   end
 end
